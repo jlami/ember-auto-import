9d70689cd0053d6dbbf829b10a041377e0257ad5
Joren l'Ami <joren@distinct.nl>
2021-06-08 14:00:36 +0200

    compiled results

--- types/broccoli-debug/index.d.ts
+++
@@ -1,8 +0,0 @@
-declare module 'broccoli-debug' {
-  import { Tree } from "broccoli-plugin";
-  export default class BroccoliDebug implements Tree {
-    constructor(inputTree: Tree, name: 'string');
-    __broccoliGetInfo__(): any;
-  }
-  export function buildDebugCallback(name: string): (inputTree: Tree, name: string) => Tree;
-}
